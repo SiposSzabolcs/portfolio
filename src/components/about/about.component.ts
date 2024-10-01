f84ebe98df1b91fa1d0d34108a0ec09f065108f4
Sípos Szabolcs <siposszabolcs10@gmail.com>
2024-10-01 11:57:27 +0200

    Abouit section added with techstack

--- src/components/about/about.component.ts
+++ src/components/about/about.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  tech = {
+    frontend: [
+      { name: 'html', icon: 'html.png' },
+      { name: 'css', icon: 'css.png' },
+      { name: 'js', icon: 'js.png' },
+      { name: 'ts', icon: 'ts.png' },
+      { name: 'bootstrap', icon: 'bootstrap.png' },
+      { name: 'react', icon: 'react.png' },
+      { name: 'angular', icon: 'angular.png' },
+      { name: 'redux', icon: 'redux.png' },
+    ],
+    backend: [
+      { name: 'node', icon: 'node.png' },
+      { name: 'express', icon: 'express.png' },
+      { name: 'java', icon: 'java.png' },
+      { name: 'springboot', icon: 'springboot.png' },
+      { name: 'jwt', icon: 'jwt.png' },
+      { name: 'python', icon: 'python.png' },
+      { name: 'mongo', icon: 'mongo.png' },
+      { name: 'mysql', icon: 'mysql.png' },
+    ],
+    miscellaneous: [
+      { name: 'git', icon: 'git.png' },
+      { name: 'github', icon: 'github.png' },
+      { name: 'postman', icon: 'postman.png' },
+      { name: 'vscode', icon: 'vscode.png' },
+    ],
+  };
 }
